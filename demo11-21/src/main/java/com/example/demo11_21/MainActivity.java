a4f522e4b529844cec8843f1ae3b9576eae4d7bf
ASUS <1061824439@qq.com>
2017-11-21 22:54:46 +0800

    1121

--- demo11-21/src/main/java/com/example/demo11_21/MainActivity.java
+++ demo11-21/src/main/java/com/example/demo11_21/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.demo11_21;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Drawable draw= ContextCompat.getDrawable(MainActivity.this,R.mipmap.login);
+        imageView.setImageDrawable(draw);
+        Handler m=new Handler();
+        m.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        },3000);
     }
 }
