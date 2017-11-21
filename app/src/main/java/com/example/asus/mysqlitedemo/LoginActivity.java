f5f15a13d4ebd66d77cd8d90ee117c8cf5e0ddb6
ASUS <1061824439@qq.com>
2017-11-21 17:30:33 +0800

    1121

--- app/src/main/java/com/example/asus/mysqlitedemo/LoginActivity.java
+++ app/src/main/java/com/example/asus/mysqlitedemo/LoginActivity.java
@@ -1,13 +1,35 @@
 package com.example.asus.mysqlitedemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btn_login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initButton();
+    }
+
+    private void initButton() {
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_login:
+                Intent intent = new Intent(this,ListViewActivity.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
