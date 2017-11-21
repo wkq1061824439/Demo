a2cfa6385b9fd884c0a4134263329401daee53bd
ASUS <1061824439@qq.com>
2017-11-21 20:18:04 +0800

    1121

--- app/src/main/java/com/example/asus/mysqlitedemo/LoginActivity.java
+++ app/src/main/java/com/example/asus/mysqlitedemo/LoginActivity.java
@@ -15,6 +15,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initButton();
+
     }
 
     private void initButton() {
